1477fd95ab976d6d9c1e1afd4f95b53c78b8c442
Israel Santizo <joseisraelss.jiss@gmail.com>
2020-08-22 19:14:01 -0600

    Se agrego JS al proyecto

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const toggle = document.querySelector(".toggle");
+toggle.addEventListener("click", () => {
+  const sharebtn = document.querySelector(".share");
+  sharebtn.classList.toggle("active");
+});
